3dd09f738f6040beda05e1e36eba731553941991
Bruno G. Souza <bruno@bgsouza.com>
2024-08-31 00:13:12 +0000

    [ADD] VPC module

--- modules/lambda/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_lambda_function" "this" {
-  function_name = var.function_name
-  s3_bucket     = var.s3_bucket
-  s3_key        = var.s3_key
-  handler       = "index.handler"
-  runtime       = "nodejs14.x"
-  role          = var.role_arn
-}
